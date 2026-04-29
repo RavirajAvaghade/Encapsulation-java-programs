fbf1968926ab24ce1eca3be403ddef97c2618ece
RavirajAvaghade <ravirajavaghade508@gmail.com>
2026-04-29 13:51:01 +0530

    Add files via upload

---
+++ StudentMain.java
@@ -0,0 +1,25 @@
+package com.nit.encapSulationProgram;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentMain 
+{
+
+	public static void main(String[] args) 
+	{
+
+	}
+
+}
+class Stu
+{
+	private String SID;
+	private String Sname;
+	
+	private List<Double> grades;
+	public Stu()
+	{
+		grades = new ArrayList();
+	}
+}
